4775e957135f77fe0141e98b0f387675709bc7e7
aidenlu <aiden_lu@wochacha.com>
2023-05-30 18:08:11 +0800

    feat: enable default settings

---
+++ notification/app_settings.py
@@ -0,0 +1,11 @@
+
+
+TINYMCE_DEFAULT_CONFIG = {
+    "tinymce_version": "6",
+    "height": 300,
+    "width": 600,
+    "menubar": True,
+    "language": "en_US",
+}
+TINYMCE_JS_URL = "https://cdn.jsdelivr.net/npm/tinymce/tinymce.min.js"
+TINYMCE_COMPRESSOR = False
--- notification/apps.py
+++ notification/apps.py
@@ -4,3 +4,10 @@ from django.apps import AppConfig
 class NotificationConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "notification"
+
+    def ready(self):
+        from . import app_settings as defaults
+        from django.conf import settings
+        for name in dir(defaults):
+            if name.isupper() and not hasattr(settings, name):
+                setattr(settings, name, getattr(defaults, name))
